cec91373959bcb443fd3e9574a378f7aed8bbdfc
Nicholas Terek <NicholasTerek1@gmail.com>
2025-10-18 23:58:35 -0400

    connection working

--- client.py
+++ client.py
@@ -1,4 +1,17 @@
-from socket import *
-from socketserver import *
+# Client.py
+import socket
 
+HOST = '127.0.0.1'
+PORT = 5000
 
+# Create a TCP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f"[CLIENT] Socket created")
+
+# Connect to the server
+client_socket.connect((HOST, PORT))
+print(f"[CLIENT] Connected to server at {HOST}:{PORT}")
+
+# Close the socket
+client_socket.close()
+print(f"[CLIENT] Disconnected from server")
--- server.py
+++ server.py
@@ -1,1 +1,25 @@
-from socket import *
+# Server.py
+import socket
+
+HOST = '127.0.0.1'  # localhost
+PORT = 5000 # Port to listen on
+MAX_CLIENTS = 3 # Maximum number of clients
+# Create a TCP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f"[SERVER] Socket created")
+
+# Bind the socket to address and port
+server_socket.bind((HOST, PORT))
+print(f"[SERVER] Socket bound to {HOST}:{PORT}")
+
+# Start listening for connections
+server_socket.listen(MAX_CLIENTS) 
+print(f"[SERVER] Server is listening for connections...")
+
+client_socket, client_address = server_socket.accept()
+print(f"[SERVER] Connection accepted from {client_address}")
+
+# Close sockets
+client_socket.close()
+server_socket.close()
+print(f"[SERVER] Server shut down")
