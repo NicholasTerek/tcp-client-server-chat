8b7418257f05a0013109d3d78f5c72a9c7def0b5
Nicholas Terek <NicholasTerek1@gmail.com>
2025-10-19 02:15:42 -0400

    add Client Numbers

--- client.py
+++ client.py
@@ -12,12 +12,16 @@ print(f"[CLIENT] Socket created")
 client_socket.connect((HOST, PORT))
 print(f"[CLIENT] Connected to server at {HOST}:{PORT}")
 
+data = client_socket.recv(1024)
+client_name = data.decode('utf-8')
+print(f"[CLIENT] Server assigned name: {client_name}")
+
 while True:
-    message = input("Enter your message: ")
+    message = input(f"{client_name}> ")
 
     if message.lower() == "exit":
         break
-    
+
     client_socket.send(message.encode('utf-8'))
     print(f"[CLIENT] Sent message: '{message}'")
 
--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import socket
 HOST = '127.0.0.1'  # localhost
 PORT = 5000 # Port to listen on
 MAX_CLIENTS = 3 # Maximum number of clients
+client_counter = 0
+
 # Create a TCP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f"[SERVER] Socket created")
@@ -15,9 +17,16 @@ print(f"[SERVER] Socket bound to {HOST}:{PORT}")
 server_socket.listen(MAX_CLIENTS) 
 print(f"[SERVER] Server is listening for connections...")
 
+client_counter += 1
+client_name = f"Client{client_counter:02d}"
+
 client_socket, client_address = server_socket.accept()
 print(f"[SERVER] Connection accepted from {client_address}")
+
+client_socket.send(client_name.encode('utf-8'))
+print(f"[SERVER] Assigned name: {client_name}")
 while True:
+    
     data = client_socket.recv(1024)
     if not data:
         print(f"[SERVER] Client disconnected")
